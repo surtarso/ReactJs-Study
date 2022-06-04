f6d3885876ebcc665a0769083b2873f0bfc5048c
surtarso <tarso.galvao@gmail.com>
2022-06-04 01:25:29 -0300

    some progress on graph component

--- Projeto - Github User App/src/components/graphs/index.js
+++ Projeto - Github User App/src/components/graphs/index.js
@@ -4,19 +4,18 @@ import useGithub from "../../hooks/github-hooks";
 const Graphs = () => {
     const { githubState } = useGithub();
 
-    const apiUrl = "https://github.com/surtarso/ReactJS-Projects/blob/main/Projeto%20-%20Github%20User%20App/public/api.html?"
-    // const apiUrl = "../../../public/api.html?";
+    // const apiUrl = "https://github.com/surtarso/ReactJS-Projects/blob/main/Projeto%20-%20Github%20User%20App/public/api.html?"
+    const apiUrl = './api.html';
 
     return (
         <iframe 
             title="Graphs"
-            src={apiUrl + githubState.user.login}
+            src={apiUrl + "?" + githubState.user.login}
             // scrolling="no"
             frameBorder="0"
             width={300}
             height={300}
             sandbox='allow-scripts allow-modal'
-            // ref={writeHTML}
         />
     )
 };
